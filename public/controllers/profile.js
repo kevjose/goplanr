487a9a5b7e3d0da88932350443424534bcb18e12
kevin jose <kevin.jose@aot-technologies.com>
2015-09-07 14:40:27 +0530

    loader for profile page

--- public/controllers/profile.js
+++ public/controllers/profile.js
@@ -1,9 +1,11 @@
 angular.module('MyApp')
     .controller('ProfileCtrl', function ($scope, $auth, toastr, Account) {
+        $scope.isProfileLoading = true;
         $scope.getProfile = function () {
             Account.getProfile()
                 .then(function (response) {
                     $scope.user = response.data;
+                    $scope.isProfileLoading = false;
                 })
                 .catch(function (response) {
                     toastr.clear();
@@ -11,9 +13,11 @@ angular.module('MyApp')
                 });
         };
         $scope.updateProfile = function () {
+            $scope.isProfileLoading =true;
             Account.updateProfile($scope.user)
                 .then(function () {
                     toastr.clear();
+                    $scope.isProfileLoading =false;
                     toastr.success('Profile has been updated');
                 })
                 .catch(function (response) {
